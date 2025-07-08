c8a00035ddd8967a9f9c17f298a13652a21413a1
Alejandra <malejamonroyz@gmail.com>
2025-07-08 13:50:38 +0200

    test 2 copia y modificacion con lambal

--- src/interfaces/Test02OtroOrden.java
+++ src/interfaces/Test02OtroOrden.java
@@ -10,12 +10,29 @@ public class Test02OtroOrden {
 	
 //ESTE MECANISMO NO SE UTILIZA
 	public static void main(String[] args) {
-	Set<String> set = new TreeSet<String>(new Comparator<String>() {   //creamos un objeto nuevo de una clase anonima que hereda de una interface (new Comparator<String>()
-		@Override
-		public int compare(String o1, String o2) {	
-			return o1.length() - o2.length();
-		}
-		});
+		
+//	Set<String> set = new TreeSet<String>(new Comparator<String>() {   //es una inferfaz funcional 
+//		public int compare(String o1, String o2) {	
+//			return o1.length() - o2.length();
+//		}
+//		});
+		
+		
+		//ahora hacemos lo de arriba con una exprecion lambal
+//		
+//		Set<String> set = new TreeSet<String> ((String o1, String o2) ->{
+//			return o2.length() - o1.length();
+//		});
+		
+		
+		//ahora lo vamos achicar un poco mas:
+		Set<String> set = new TreeSet<String> ((o1, o2) -> o2.length() - o1.length());
+		//como sabemos lo que va a devolver puedo quitar el tipo de dato ya que esta definido afuera, tambien podemos quitar los {} el cuerpo del metodo
+		//si el metodo tiene solo una linea puedes quitar el retour y quitar el punto y coma ;
+		//si hay dos lineas de codigo si es obligacion hacerlo como el de arriba 
+		//al TreeSet le estamoa pasando una funcion 
+	
+		
 			
 			set.add("uno");
 			set.add("dos");
@@ -30,10 +47,13 @@ public class Test02OtroOrden {
 			set.add("Ávila");
 			set.add("ágil");
 			
-			for(String s: set) {
-				System.out.println(s);
-			}
-		
+//			for(String s: set) {
+//				System.out.println(s);
+//			}
+			 set.forEach((s) -> System.out.println(s));
+			 
+			 //otra exprecion lamdal
+			 set.forEach(System.out::println); //en vez de ponerle la implementacion del metodo, le pongo un metodo que hace lo mismo de la implementacion
 		
 	}
 
